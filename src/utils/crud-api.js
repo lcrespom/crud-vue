abbee8ab0e210008da9459c31937db51cd2493af
Luis Crespo <lcrespo@es.ibm.com>
2017-02-02 13:11:15 +0100

    PUT is working

--- src/utils/crud-api.js
+++ src/utils/crud-api.js
@@ -7,24 +7,31 @@ const crudApi = {
 	},
 
 	post(apicfg, route, data) {
-		return fetch(getURL(apicfg, 'post', route), {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: new Headers({
-				'Content-Type': 'application/json'
-			})
-		})
+		let url = getURL(apicfg, 'post', route);
+		return fetch(url, fetchInit('POST', data))
 		.then(response => response.json());
 	},
 
 	put(apicfg, route, data) {
-		console.error('Not implemented');
+		let url = getURL(apicfg, 'post', route);
+		return fetch(url + '/' + data._id, fetchInit('PUT', data))
+		.then(response => response.json());
 	}
 };
 
 function getURL(apicfg, name, route) {
 	let suffix = apicfg[name] ? apicfg[name] : route;
 	return apicfg.prefix + suffix;
+}
+
+function fetchInit(method, data) {
+	return {
+		method,
+		body: JSON.stringify(data),
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		})
+	};
 }
 
 export default crudApi;
